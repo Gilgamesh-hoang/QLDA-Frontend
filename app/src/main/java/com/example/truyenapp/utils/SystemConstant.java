391f51d73912aa05a9b380e7d7a8b7550ddd0867
Le Anh Duc <ducvui2003@gmail.com>
2024-10-16 00:26:21 +0700

    ref: login

---
+++ app/src/main/java/com/example/truyenapp/utils/SystemConstant.java
@@ -0,0 +1,13 @@
+package com.example.truyenapp.utils;
+
+public class SystemConstant {
+    public static final String SHARED_PREF_NAME = "MyPrefs";
+    public static final String JWT_TOKEN = "token";
+    public static final String ROLE_USER = "USER";
+    public static final String ROLE_ADMIN = "ADMIN";
+    public static final String STATUS_FULL_KEY = "FULL";
+    public static final String STATUS_UPDATING_KEY = "UPDATING";
+    public static final String STATUS_FULL_VALUE = "Hoàn thành";
+    public static final String STATUS_UPDATING_VALUE = "Đang cập nhật";
+
+}
