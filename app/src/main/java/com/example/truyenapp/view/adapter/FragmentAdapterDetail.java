e02f7146f0d0160a2bc2915778be73f04860fed3
Lê Anh Đức <ducvui2003@gmail.com>
2024-10-19 18:41:06 +0700

    feat: show comic in home fragment (#4)
    feat: show rank and category

--- app/src/main/java/com/example/truyenapp/view/adapter/FragmentAdapterDetail.java
+++ app/src/main/java/com/example/truyenapp/view/adapter/FragmentAdapterDetail.java
@@ -6,7 +6,6 @@ import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-//import com.example.truyenapp.view.fragment.ChapterFragment;
 import com.example.truyenapp.view.fragment.ChapterFragment;
 import com.example.truyenapp.view.fragment.DetailFragment;
 
