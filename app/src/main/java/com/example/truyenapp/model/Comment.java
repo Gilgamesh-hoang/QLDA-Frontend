1f2fcb12f35d4598f9ec958b8c56cc3568b764ea
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-10-28 01:14:00 +0700

    add DTOs and modal for comment functionality.

---
+++ app/src/main/java/com/example/truyenapp/model/Comment.java
@@ -0,0 +1,25 @@
+package com.example.truyenapp.model;
+
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Comment {
+    private int id, idChapter, idAccount;
+    private String content, postingDay;
+    private int state;
+    private String bookName;
+    private String chapterName;
+    private Date createdAt;
+    private String thumbnail;
+    private String email;
+    private String username;
+    private String avatar;
+}
